9d657731e545e9bfbdd3463807fe369b38e799cd
Tallerik <Tallerik@users.noreply.github.com>
2017-08-09 08:58:05 +0200

    Add files via upload

--- Easy_Gamemode/src/me/Tallerik/EasyGamemode/EasyGamemode.java
+++ Easy_Gamemode/src/me/Tallerik/EasyGamemode/EasyGamemode.java
@@ -1,9 +1,10 @@
 package me.Tallerik.EasyGamemode;
 
-import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EasyGamemode extends JavaPlugin
@@ -19,11 +20,27 @@ public class EasyGamemode extends JavaPlugin
 	
 	public boolean onCommand(CommandSender  sender, Command cmd, String label, String[] args)
 	{
+		Player player = (Player) sender;
+		if(cmd.getName().equalsIgnoreCase("easygamemode"))
+		{			
+			if(args.length == 0)
+			{
+				player.sendMessage(prefix + " §fEasyGamemode version: §6" + getDescription().getVersion() + ".");
+				player.sendMessage(prefix + " §fBenutze §6 /easygamemode help §fum die Befehle einzusehen. ");
+			}
+			else
+			{
+				player.sendMessage(prefix + " §2/c§b  Kreativemodus");
+				player.sendMessage(prefix + " §2/s§b  Überlebensmodus");
+				player.sendMessage(prefix + " §2/h§b  Hardcoremodus");
+				player.sendMessage(prefix + " §2/sp§b Spectatormodus");
+			}
+		}
 		if(cmd.getName().equalsIgnoreCase("s"))
 		{
-			if(sender.hasPermission("EasyGM.s") && sender.hasPermission("EasyGM.cmd"))
+			if(sender.hasPermission("EasyGM.s") || sender.hasPermission("EasyGM.cmd"))
 			{
-				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "gamemode 0 " + sender.getName());
+				player.setGameMode(GameMode.SURVIVAL);
 				sender.sendMessage(prefix + "§bDu bist nun im Überlebensmodus!");
 			}
 			else
@@ -33,10 +50,34 @@ public class EasyGamemode extends JavaPlugin
 		}
 		if(cmd.getName().equalsIgnoreCase("c"))
 		{
-			if(sender.hasPermission("EasyGM.c") && sender.hasPermission("EasyGM.cmd"))
+			if(sender.hasPermission("EasyGM.c") || sender.hasPermission("EasyGM.cmd"))
 			{
-				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "gamemode 1 " + sender.getName());
+				player.setGameMode(GameMode.CREATIVE);
 				sender.sendMessage(prefix + "§bDu bist nun im Kreativmodus!");
+			}
+			else
+			{
+				sender.sendMessage(prefix + "§cDu hasst dazu keine Rechte");
+			}
+		}
+		if(cmd.getName().equalsIgnoreCase("h"))
+		{
+			if(sender.hasPermission("EasyGM.s") || sender.hasPermission("EasyGM.cmd"))
+			{
+				player.setGameMode(GameMode.ADVENTURE);
+				sender.sendMessage(prefix + "§bDu bist nun im HardCoremodus!");
+			}
+			else
+			{
+				sender.sendMessage(prefix + "§cDu hasst dazu keine Rechte");
+			}
+		}
+		if(cmd.getName().equalsIgnoreCase("sp"))
+		{
+			if(sender.hasPermission("EasyGM.s") || sender.hasPermission("EasyGM.cmd"))
+			{
+				player.setGameMode(GameMode.SPECTATOR);
+				sender.sendMessage(prefix + "§bDu bist nun im Spectatormodus!");
 			}
 			else
 			{
